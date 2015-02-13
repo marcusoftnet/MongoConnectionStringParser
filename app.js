377004ea59f30ee9622b157b53c7103ebb23ced8
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-02-13 14:58:07 +0700

    Using static-now

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-var app = require("koa")();
-var serve = require("koa-static");
-
-var directory = __dirname;
-var portNo = process.env.PORT || 3000;
-
-app.use(serve(directory));
-
-app.listen(portNo);
-
-console.log("Serving files from      : " + directory);
-console.log("Your site is running at : http://localhost:" + portNo);
+var app = require("static-now");
+var options = { portnumber : process.env.PORT || 3000 };
+app(options);
