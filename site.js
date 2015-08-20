7539448a4f8b473d90c6ea2e94345b8699bb3271
Marcus Hammarberg <marcusoft.net@gmail.com>
2015-08-20 09:33:48 +0700

    Added database

--- site.js
+++ site.js
@@ -14,6 +14,8 @@ $(document).ready(function () {
 		var userParts = protocolParts[1].split('@')[0];
 		var user = userParts.split(':')[0];
 		var password = userParts.split(':')[1];
+		
+		var database = connectionString.substring(connectionString.lastIndexOf("/")+1);
 
 		$('#protocol').val(protocol);
 		$('#copy-button-protocol').attr('data-clipboard-text', protocol);
@@ -26,12 +28,15 @@ $(document).ready(function () {
 
 		$('#user').val(user);
 		$('#copy-button-user').attr('data-clipboard-text', user);
+		
+		$('#database').val(database);
+		$('#copy-button-database').attr('data-clipboard-text', database);
 
 		$('#password').val(password);
 		$('#copy-button-password').attr('data-clipboard-text', password);
 
 		$('#copy-button-all').attr('data-clipboard-text',
-			createParsedConnectionString(protocol, server, port, user, password, connectionString));
+			createParsedConnectionString(protocol, server, port, user, database, password, connectionString));
 	});
 
 	var clientPort = new ZeroClipboard(document.getElementById("copy-button-port"));
@@ -39,6 +44,7 @@ $(document).ready(function () {
 	var clientProtocol = new ZeroClipboard(document.getElementById("copy-button-protocol"));
 	var clientPassword = new ZeroClipboard(document.getElementById("copy-button-password"));
 	var clientUser = new ZeroClipboard(document.getElementById("copy-button-user"));
+	var clientDatabase = new ZeroClipboard(document.getElementById("copy-button-database"));
 	var clientAll = new ZeroClipboard(document.getElementById('copy-button-all'));
 
 	clientPort.on("ready", function (r) { hookUpMessage(clientPort)});
@@ -46,12 +52,13 @@ $(document).ready(function () {
 	clientProtocol.on("ready", function (r) { hookUpMessage(clientProtocol)});
 	clientPassword.on("ready", function (r) { hookUpMessage(clientPassword)});
 	clientUser.on("ready", function (r) { hookUpMessage(clientUser)});
+	clientDatabase.on("ready", function (r) { hookUpMessage(clientDatabase)});
 	clientAll.on("ready", function (r) { hookUpMessage(clientAll)});
 
 	var hookUpMessage = function (client) {
 		client.on("aftercopy", function(e){
 			$("#message")
-				.html("Copied... paste where you like")
+				.html("Copied to your clipboard")
 				.removeClass('hidden')
 				.fadeIn(500)
 				.delay(2000)
@@ -59,12 +66,13 @@ $(document).ready(function () {
 		});
 	};
 
-	var createParsedConnectionString = function (protocol, server, port, user, password, connectionString) {
+	var createParsedConnectionString = function (protocol, server, port, user, database, password, connectionString) {
 		var parts = [
 			"Protocol   : " + protocol,
 			"Server     : " + server,
 			"Port       : " + port,
 			"User       : " + user,
+			"Database   : " + database,
 			"Password   : " + password,
 			" ",
 			"Connection : " + connectionString];
